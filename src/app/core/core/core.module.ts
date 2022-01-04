9fe18af20c5fde2828db53e8ccb7f98934819249
Big Maks <Big Maks>
2022-01-04 11:28:35 +0200

    added translations lib

--- src/app/core/core/core.module.ts
+++ src/app/core/core/core.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  TranslateModule,
+  TranslateLoader,
+  TranslateStore,
+} from '@ngx-translate/core';
 import { LocalStorageService } from './services/local-storage.service';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  IModuleTranslationOptions,
+  ModuleTranslateLoader,
+} from '@larscom/ngx-translate-module-loader';
+
+export function moduleHttpLoaderFactory(http: HttpClient) {
+  const baseTranslateUrl = './assets/i18n';
+
+  const options: IModuleTranslationOptions = {
+    modules: [
+      // final url: ./assets/i18n/en.json
+      { baseTranslateUrl },
+      // final url: ./assets/i18n/home/test/en.json
+      { baseTranslateUrl, moduleName: 'home/test' },
+    ],
+  };
+
+  return new ModuleTranslateLoader(http, options);
+}
 
 @NgModule({
-  imports: [HttpClientModule],
+  imports: [
+    HttpClientModule,
+    TranslateModule.forChild({
+      defaultLanguage: 'ru',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: moduleHttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   declarations: [],
-  providers: [LocalStorageService],
-  exports: [HttpClientModule],
+  providers: [LocalStorageService, TranslateStore],
+  exports: [HttpClientModule, TranslateModule],
 })
 export class CoreModule {}
